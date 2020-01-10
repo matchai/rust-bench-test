3c7f6619ef331251e3adce3a5e44ab4393f7e99f
Matan Kushner <hello@matchai.dev>
2020-01-10 17:38:28 -0500

    Initial commit

---
+++ benches/benches.rs
@@ -0,0 +1,20 @@
+#![feature(test)]
+
+extern crate test;
+
+use starship_test::fibonacci;
+use test::Bencher;
+
+#[bench]
+fn bench_fib_10(b: &mut Bencher) {
+    b.iter(|| {
+        let _ = fibonacci(10);
+    });
+}
+
+#[bench]
+fn bench_fib_20(b: &mut Bencher) {
+    b.iter(|| {
+        let _ = fibonacci(20);
+    });
+}
