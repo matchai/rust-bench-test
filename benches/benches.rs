b65ebf59262bb2dcb6fb5f359628fabd49e2f4fc
Matan Kushner <hello@matchai.dev>
2020-01-10 18:18:35 -0500

    Rerun fibonacci function

--- benches/benches.rs
+++ benches/benches.rs
@@ -9,6 +9,9 @@ use test::Bencher;
 fn bench_fib_10(b: &mut Bencher) {
     b.iter(|| {
         let _ = fibonacci(10);
+        let _ = fibonacci(10);
+        let _ = fibonacci(10);
+        let _ = fibonacci(10);
     });
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 pub fn fibonacci(num: u32) -> u32 {
-    let _ = num * num * num * num * num * num * num * num * num * num * num;
-
     match num {
         0 | 1 => 1,
         _ => fibonacci(num - 2) + fibonacci(num - 1)
