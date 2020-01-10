a43b16ce388e394782de4106011f326187f63ec9
Matan Kushner <hello@matchai.dev>
2020-01-10 17:54:26 -0500

    Add sleep for 1 sec

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+use std::{time, thread};
+
 pub fn fibonacci(num: u32) -> u32 {
+    thread::sleep(time::Duration::from_secs(1));
+
     match num {
         0 | 1 => 1,
         _ => fibonacci(num - 2) + fibonacci(num - 1)
