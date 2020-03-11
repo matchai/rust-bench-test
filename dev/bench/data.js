78a8df73dbaeb9b309a332b3c019ff237116358a
github-action-benchmark <github@users.noreply.github.com>
2020-03-11 04:06:41 +0000

    add Rust Benchmark (cargo) benchmark result for 5adc1f8c3c4f0fe14f9f6c9b721205fd43386661

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1583899601046,
+  "repoUrl": "https://github.com/matchai/rust-bench-test",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "hello@matchai.dev",
+            "name": "Matan Kushner",
+            "username": "matchai"
+          },
+          "committer": {
+            "email": "hello@matchai.dev",
+            "name": "Matan Kushner",
+            "username": "matchai"
+          },
+          "distinct": true,
+          "id": "5adc1f8c3c4f0fe14f9f6c9b721205fd43386661",
+          "message": "Empty commit",
+          "timestamp": "2020-03-11T00:06:10-04:00",
+          "tree_id": "2e8cbba03f343958d1bb2c8d562acbf78571e897",
+          "url": "https://github.com/matchai/rust-bench-test/commit/5adc1f8c3c4f0fe14f9f6c9b721205fd43386661"
+        },
+        "date": 1583899600708,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_fib_10",
+            "value": 223,
+            "range": "+/- 50",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_fib_20",
+            "value": 29811,
+            "range": "+/- 3,575",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
