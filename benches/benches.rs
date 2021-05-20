f0cb7a75d87e2b96a02e969f91a7777cd0d4786e
Matan Kushner <hello@matchai.dev>
2021-05-19 20:10:13 -0400

    Migrate to criterion

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,20 +1,9 @@
-#![feature(test)]
-
-extern crate test;
-
 use rust_bench_test::fibonacci;
-use test::Bencher;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-#[bench]
-fn bench_fib_10(b: &mut Bencher) {
-    b.iter(|| {
-        let _ = fibonacci(10);
-    });
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
 }
 
-#[bench]
-fn bench_fib_20(b: &mut Bencher) {
-    b.iter(|| {
-        let _ = fibonacci(20);
-    });
-}
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
